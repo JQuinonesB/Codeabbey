cdf6e1c94e6f64516cf85472385c426ebe3cf26d
Jairo <jairoa.quinonesb@gmail.com>
2022-06-12 15:59:12 -0500

    Update

--- Codeabbey002/main.c
+++ Codeabbey002/main.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
 
+#define MAX_ARR_SIZE 500
+
+int read_array(int tot_arr, int count, int *ptr2arr);
+
 int main(){
-    int tot_arr, sum2, res;
-    scanf("%d", &tot_arr);
-    res = sum1 + sum2;
-    printf("%d\n", res);
+    int nums_array[MAX_ARR_SIZE];
+    int counter = 0, total_arr, *ptr_array;
+
+    scanf("%d", &total_arr);
+    read_array(total_arr, counter, &nums_array);
     return 0;
+}
+
+int read_array(int tot_arr, int count, int *ptr2arr){
+    if(count == tot_arr){
+        return 0;
+    }
+    else{
+        scanf("%d", *ptr2arr);
+        count++;
+        ptr2arr++;
+    }
+
 }
