b65e9834e437af5bdc827418efe44aa73b18b7ad
Jairo <jairoa.quinonesb@gmail.com>
2022-06-12 18:06:47 -0500

    Update

--- Codeabbey004/main.c
+++ Codeabbey004/main.c
@@ -2,20 +2,19 @@
 
 #define MAX_ARRAY_SIZE 500
 
-void read_array(int tot_arr, int count);
+void read_array(int tot_arr, int count, int *ptr_min);
 
 int main(){
     int counter = 0, total_arr, suma = 0;
-    int num1_array[MAX_ARRAY_SIZE], num2_array[MAX_ARRAY_SIZE];
     int min_array[MAX_ARRAY_SIZE];
 
     scanf("%d", &total_arr);
-    read_array(total_arr, counter);
+    read_array(total_arr, counter, &min_array);
     printf("%d\n", suma);
     return 0;
 }
 
-void read_array(int tot_arr, int count){
+void read_array(int tot_arr, int count, int *ptr_min){
     int temp1, temp2;
     if(count == tot_arr){
 
@@ -23,9 +22,13 @@ void read_array(int tot_arr, int count){
     else{
         scanf("%d", &temp1);
         scanf("%d", &temp2);
-        printf("%d\n", (temp1 + temp2));
+        (temp1 <= temp2) ?
+            (*ptr_min = temp1):
+            (*ptr_min = temp2);
+
+        ptr_min++;
         count++;
-        read_array(tot_arr, count);
+        read_array(tot_arr, count, &ptr_min);
     }
 
 }
