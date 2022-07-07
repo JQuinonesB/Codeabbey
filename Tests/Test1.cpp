ffe4239ce63b11c1bc00e1b3a5853683160b46c1
Jairo <jairoa.quinonesb@gmail.com>
2022-07-07 17:13:51 -0500

    Test

---
+++ Tests/Test1.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+#include <csignal>
+
+#include <unistd.h>
+
+
+
+using namespace std;
+
+
+
+void signalHandler( int signum ) {
+
+   cout << "Interrupt signal (" << signum << ") received.\n";
+
+
+
+   // cleanup and close up stuff here
+
+   // terminate program
+
+
+
+   exit(signum);
+
+}
+
+
+
+int main () {
+
+   int i=0;
+
+
+   signal(SIGINT, signalHandler);
+
+
+
+   while(++i) {
+
+      cout << "Going to sleep...." << endl;
+
+      if( i == 3 ) {
+
+         raise( SIGINT);
+
+      }
+
+      usleep(1000000);
+
+   }
+
+
+
+   return 0;
+
+}
