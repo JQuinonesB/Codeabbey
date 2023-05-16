a9d67bf3452cae8d3e9d7483a32e8db53baca004
Jairo <jairoa.quinonesb@gmail.com>
2023-05-16 10:13:46 +0200

    First Compilation

--- Codeabbey003/main.c
+++ Codeabbey003/main.c
@@ -4,23 +4,26 @@
 
 void read_array(int tot_arr, int count);
 
-int main(){
+void main()
+{
     int counter = 0, total_arr, suma = 0;
     int num1_array[MAX_ARRAY_SIZE], num2_array[MAX_ARRAY_SIZE];
     int min_array[MAX_ARRAY_SIZE];
 
     scanf("%d", &total_arr);
     read_array(total_arr, counter);
-    printf("%d\n", suma);
-    return 0;
+    return;
 }
 
-void read_array(int tot_arr, int count){
+void read_array(int tot_arr, int count)
+{
     int temp1, temp2;
-    if(count == tot_arr){
-
+    if(count == tot_arr)
+    {
+        return;
     }
-    else{
+    else
+    {
         scanf("%d", &temp1);
         scanf("%d", &temp2);
         printf("%d\n", (temp1 + temp2));
